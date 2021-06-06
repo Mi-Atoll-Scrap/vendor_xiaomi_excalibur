e41050bfccad5789f441ae01fd38a2103ba27623
mrjarvis698 <dudamajaykumar59@gmail.com>
2021-06-06 18:52:48 +0530

    excalibur: pull UBWC blobs from stock

--- excalibur-vendor.mk
+++ excalibur-vendor.mk
@@ -2239,3 +2239,7 @@ PRODUCT_COPY_FILES += \
     vendor/xiaomi/excalibur/proprietary/vendor/lib64/libhbtpclient.so:$(TARGET_COPY_OUT_VENDOR)/lib64/libhbtpclient.so \
     vendor/xiaomi/excalibur/proprietary/vendor/lib64/libhbtpdsp.so:$(TARGET_COPY_OUT_VENDOR)/lib64/libhbtpdsp.so \
     vendor/xiaomi/excalibur/proprietary/vendor/lib64/libhbtpfrmwk.so:$(TARGET_COPY_OUT_VENDOR)/lib64/libhbtpfrmwk.so
+
+# UBWC
+PRODUCT_COPY_FILES += \
+    vendor/xiaomi/excalibur/proprietary/vendor/lib/libstreamparser.so:$(TARGET_COPY_OUT_VENDOR)/lib/libstreamparser.so
